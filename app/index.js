de89caa0fab04491dea10f89adc57cdaf5a4182e
Caio Nascimento - Ubuntu <caionsantos2000@gmail.com>
2021-12-30 21:39:26 -0300

    feat: cors middleare

--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const sequelize = require("./utils/db");
 const route = require("./routes/routes");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(route);
 
 const PORT = 3000;
